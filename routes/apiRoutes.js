d25ad0d7b3bdb18e7031772fbc69cf7a215b9bb9
Jasmine Kay <jasminekay505@gmail.com>
2021-03-08 11:50:38 -0800

    Get request in apiRoutes.js is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,12 @@
 const fs = require('fs');
 
 //Routing
-module.exports = (app) =>  { 
+module.exports = (app) => {
     //GET Request
-    app.get('/api/notes', (req, res) =>  { 
-        let notes = JSON.parse(fs.readFileSync('../db/db.json'));
-        res.json(notes)            
-        });
+    app.get('/api/notes', (req, res) => {
+        let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        res.json(notes)
+    });
     //POST Request
 
     //DELETE Request
