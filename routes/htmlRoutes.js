2d555ead88f56888dacc17c7b4493b4012c9929b
Jasmine Kay <jasminekay505@gmail.com>
2021-03-08 11:47:24 -0800

    Updating HTML routes, checked in Insomnia that they work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,16 +2,16 @@
 const path = require('path');
 
 //Routing
-module.exports = (app) => { 
+module.exports = (app) => {
     //GET Requests
-    app.get('/', (req,res) => { 
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../index.html'));
     });
-    app.get('/notes', (req,res) => { 
+    app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../notes.html'));
     });
     //Default
-    app.get('*', (req,res) => { 
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../index.html'));
     });
 }
