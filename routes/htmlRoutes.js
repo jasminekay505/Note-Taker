520499d9021eab8e0c5c33d996d9ac95c0dd8c34
Jasmine Kay <jasminekay505@gmail.com>
2021-03-08 12:55:35 -0800

    Added routes to .js and .css files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 //Dependencies
 const path = require('path');
+const { traceDeprecation } = require('process');
 
 //Routing
 module.exports = (app) => {
@@ -9,6 +10,12 @@ module.exports = (app) => {
     });
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+    app.get('/assets/css/styles.css', (req,res) => { 
+        res.sendFile(path.join(__dirname, '../public/assets/css/styles.css'));
+    });
+    app.get('/assets/js/index.js', (req,res) => { 
+        res.sendFile(path.join(__dirname, '../public/assets/js/index.js'));
     });
     //Default
     app.get('*', (req, res) => {
