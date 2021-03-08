a23d18a80152a81dddf189a30515f594e4bc995a
Jasmine Kay <jasminekay505@gmail.com>
2021-03-08 12:12:50 -0800

    Setting up POST request in APIroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,21 @@
 //Dependencies
 const fs = require('fs');
+let notes = JSON.parse(fs.readFileSync('./db/db.json'));
 
 //Routing
 module.exports = (app) => {
     //GET Request
     app.get('/api/notes', (req, res) => {
-        let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
         res.json(notes)
     });
     //POST Request
-
+    app.post('/api/notes', (req,res) => { 
+        let newNote = req.body;
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', stringData, (err, data) => { 
+            if (err) throw err;
+        });
+        res.send('The new note has been added');
+    })
     //DELETE Request
 }
