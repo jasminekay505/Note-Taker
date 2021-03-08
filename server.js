b5fabad733deb77ce86c582f365ed8d688891dfe
Jasmine Kay <jasminekay505@gmail.com>
2021-03-08 11:12:36 -0800

    Setting up server, routers and listener

---
+++ server.js
@@ -0,0 +1,21 @@
+//Dependencies
+const express = require('express');
+
+//Express configuration
+const app = express();
+
+//Set up port
+const PORT = process.env.PORT || 8080;
+
+//Set up Express app to handle data parsing
+app.use(express.urlendcoded({extended: true}));
+app.use(express.json());
+
+//Router
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//Listener
+app.listen(PORT, () => { 
+    console.log(`App is listening on PORT: ${PORT}`);
+});
